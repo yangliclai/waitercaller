e50bccd815ac6c916ae52874545c80938ebc5eb3
yangliclai <yangliclai@gmail.com>
2018-04-22 13:05:23 -0400

    05 again

--- waitercaller.py
+++ waitercaller.py
@@ -127,15 +127,15 @@ def account_createtable():
         tableid = DB.add_table(form.tablenumber.data, current_user.get_id())
         new_url = BH.shorten_url(config.base_url + "newrequest/" + str(tableid))
         DB.update_table(tableid, new_url)
-        flag = 1 
+        #flag = 1 
         #tableid02 = DB.add_table_fulltest(form.tablenumber.data, current_user.get_id(),new_url)
         #DB.delete_table_fulltest(tableid02)
         
         return redirect(url_for('account'))
-    if flag = 1:
-        tableid02 = DB.add_table_fulltest(form.tablenumber.data, current_user.get_id(),new_url)
-        DB.delete_request_redundancy(tableid)
-        flag = 0
+    #if flag = 1:
+    #    tableid02 = DB.add_table_fulltest(form.tablenumber.data, current_user.get_id(),new_url)
+    #    DB.delete_request_redundancy(tableid)
+    #    flag = 0
     return render_template("account.html", createtableform=form, tables=DB.get_tables(current_user.get_id()))
 
 @app.route("/account/deletetable")
